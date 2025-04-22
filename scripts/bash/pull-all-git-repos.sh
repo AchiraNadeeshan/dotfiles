93901ce1a6de981c162ad9538345f7a29989845e
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-04-22 12:23:37 +0530

    feat(scripts): add bash script to pull all git repos in current directory

---
+++ scripts/bash/pull-all-git-repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_DIR=$(pwd)
+
+echo "Searching for Git repositories in $BASE_DIR..."
+echo
+
+for dir in */; do
+  if [ -d "$dir/.git" ]; then
+    echo "Pulling latest changes in $dir..."
+    (
+      cd "$dir" && git pull
+    )
+    echo
+  fi
+done
+
+echo "All git repositories in $BASE_DIR updated."
