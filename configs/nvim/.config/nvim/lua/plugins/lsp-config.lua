0c8c012421da362b6d25d6fd16c77dd7572b5bca
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-05-05 15:24:27 +0530

    fix(nvim): add proper lsp settings for lua_ls and correct ts_ls setup

--- configs/nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ configs/nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,57 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            auto_install = true,
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+            -- Lua
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim" } },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
+                        },
+                        telemetry = { enable = false },
+                    },
+                },
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-		end,
-	},
+            -- HTML
+            lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+
+            -- TypeScript
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
+
+            -- LSP keymaps
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        end,
+    },
 }
