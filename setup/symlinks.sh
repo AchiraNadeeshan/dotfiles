94486faae0cfe69939cc1b81107ea1c9bbf6dff7
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-05-06 17:07:55 +0530

    feat(setup): add symlink creation script for Git Bash

---
+++ setup/symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create symlink for .bashrc
+ln -sf "$HOME/dotfiles/configs/shell/.bashrc" "$HOME/.bashrc"
+
+# Create symlink for .bash_profile
+ln -sf "$HOME/dotfiles/configs/shell/.bash_profile" "$HOME/.bash_profile"
+
+# Create symlink for Neovim config (Windows-specific path)
+ln -sf "$HOME/dotfiles/configs/nvim/.config/nvim" "/c/Users/$USERNAME/AppData/Local/nvim"
+
+# Create symlink for Obsidian vault config
+ln -sf "$HOME/dotfiles/configs/obsidian/.obsidian" "/d/Downloads/obsidian-vault/.obsidian"
+
+# Create symlink for Alacritty config (Windows-specific path)
+ln -sf "$HOME/dotfiles/configs/alacritty" "/c/Users/$USERNAME/AppData/Roaming/alacritty"
+
